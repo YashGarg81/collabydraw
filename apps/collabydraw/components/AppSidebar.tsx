71c4d87780fa362ae2ad05be96c4e0f7109130be
Yash Garg <yash.garg.cse.2021@miet.ac.in>
2026-05-15 19:01:31 +0530

    fix: update LinkedIn profile link to yash-garg-78a448312

--- apps/collabydraw/components/AppSidebar.tsx
+++ apps/collabydraw/components/AppSidebar.tsx
@@ -160,7 +160,7 @@ export function AppSidebar({ isOpen, onClose, canvasColor, setCanvasColor, isMob
                                 <Github className="h-4 w-4" />GitHub - <span className="flex items-center justify-start gap-1">{stars}<Star className="h-4 w-4" /></span>
                             </Link>
                             <SidebarLinkItem icon={Twitter} label="Twitter / X" url="https://x.com/1omsharma" />
-                            <SidebarLinkItem icon={Linkedin} label="Linkedin" url="https://www.linkedin.com/in/1omsharma" />
+                            <SidebarLinkItem icon={Linkedin} label="Linkedin" url="https://www.linkedin.com/in/yash-garg-78a448312" />
                         </nav>
                     </div>
 
@@ -255,3 +255,4 @@ function SidebarLinkItem({ icon: Icon, label, shortcut, className, url }: Sideba
         </Link>
     )
 }
+
